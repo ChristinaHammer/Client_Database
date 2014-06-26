8d724f4113dfc85f018c98e61090923df8429d5c
Noelle Todd <decorationgirl@gmail.com>
2014-06-26 10:52:42 -0400

    Update cdbfunctions.py
    Added an "age" attribute to the oldClientData class; added a list_visits function that will return a list of information for the past three visits, added update_volunteer, delete_person, and delete_volunteer functions.

--- cdbfunctions.py
+++ cdbfunctions.py
@@ -1,7 +1,7 @@
 """cdbfunctions.py
 
 Developer: Noelle Todd
-Last Updated: June 24, 2014
+Last Updated: June 26, 2014
 
 This module consists of helper functions which will be called by another
 module that will called directly by the user interface. This module is 
@@ -11,6 +11,7 @@ edited in the following weeks.
 """
 
 from sqlalchemy import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy import desc
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -22,7 +23,7 @@ from cdbtabledef import Household, Person, Volunteer, Visit
 #objects to hold data for function calls
 class newClientData:
 	def __init__(self, firstname, lastname, dob, phone=None,
-				dateJoined=datetime.now()):
+			dateJoined=datetime.now()):
 		self.firstname = str(firstname)
 		self.lastname = str(lastname)
 		self.dob = dob
@@ -31,17 +32,18 @@ class newClientData:
 
 class oldClientData:
 	def __init__(self, id, firstname, lastname, dob, phone=None,
-				dateJoined=datetime.now()):
+			dateJoined=datetime.now()):
 		self.id = id
 		self.firstname = str(firstname)
 		self.lastname = str(lastname)
 		self.dob = dob
+		self.age = age(dob)
 		self.phone = str(phone)
 		self.dateJoined = dateJoined
 
 class houseData:
 	def __init__(self, street, city='Troy', state='NY', zip='12180', 
-				dateVerified=None, apt=None):
+			dateVerified=None, apt=None):
 		self.street = street
 		self.city = city
 		self.state = state
@@ -55,6 +57,33 @@ class visitData:
 		self.visitDate = visitDate
 		self.notes = notes
 		
+		
+def list_visits(I_ID):
+	"""This function will find the past three visits for a household
+	and return them. NOTE: we also need the name of the person who picked up
+	
+	"""
+	visits = []
+	pers = s.query(Person).filter(Person.id == I_ID).one()
+	house = s.query(Household).filter(Household.id == pers.HH_ID).one()
+	
+	#returns all visits for the household in descending order of date
+	visithistory = s.query(Visit, Person, Volunteer).\
+				filter(Visit.HH_ID == house.id).\
+				filter(Visit.I_ID == Person.id).\
+				filter(Visit.Vol_ID == Volunteer.id).\
+				order_by(desc(Visit.date)).all()
+	
+	#retrieves information for past three visits and returns in a list
+	for instance in visithistory[0:3]:
+		clientname = instance.Person.first_name + " " +\
+				instance.Person.last_name
+		volname = instance.Volunteer.first_name + " " +\
+				instance.Volunteer.last_name
+		visits.append((instance.Visit.date, clientname, volname,
+				instance.Visit.visit_notes))
+						
+	return visits
 
 
 #Functions for inserts
@@ -80,7 +109,7 @@ def insert_person(s, firstname, lastname, dob, datejoined, newhouse,
 	
 	"""
 	newpers = Person(first_name=firstname, last_name=lastname,
-					  DOB=dob, date_joined=datejoined, phone=phonenum)
+			DOB=dob, date_joined=datejoined, phone=phonenum)
 	newpers.HH_ID = newhouse
 	newpers.age = age(dob)
 	s.add(newpers)
@@ -94,26 +123,26 @@ def insert_volunteer(s, firstname, lastname, phonenum):
 	
 	"""
 	new_vol = Volunteer(first_name=firstname, last_name=lastname, 
-						phone=phonenum)
+				phone=phonenum)
 	s.add(new_vol)
 	s.commit()
 	
 
 def insert_visit(s, Vol_id, pers_id, house_id, date_of_visit=datetime.now(),
-				notes=None):
+		notes=None):
 	"""This function creates a new row in the Visit table to hold
 	the data for a visit.
 	
 	"""
 	new_visit = Visit(I_ID=pers_id, HH_ID=house_id, Vol_ID=Vol_id,
-					date=date_of_visit, visit_notes=notes)
+			date=date_of_visit, visit_notes=notes)
 	s.add(new_visit)
 	s.commit()
 	
 
 #functions for updating records
 def update_household(s, HH_ID, street, city, state, zip, apt=None,
-					date_verified=None):
+			date_verified=None):
 	"""This function will update a households records
 	"""
 	house = s.query(Household).filter(Household.id == HH_ID).one()
@@ -137,6 +166,36 @@ def update_person(s, I_ID, firstname, lastname, dob):
 	s.commit()
 	
 
+def update_volunteer(s, vol_id, firstname, lastname, phonenum):
+	"""This function will update a volunteer's records.
+	"""
+	vol = s.query(Volunteer).filter(Volunteer.id == vol_id).one()
+	vol.first_name = firstname
+	vol.last_name = lastname
+	vol.phone = phonenum
+	s.commit()
+	
+	
+#functions for deletes
+def delete_household(s, HH_ID): pass
+
+
+def delete_person(s, I_ID):
+	"""This function will delete an individual from the database.
+	"""
+	pers = s.query(Person).filter(Person.id == I_ID).one()
+	s.delete(pers)
+	s.commit()
+	
+	
+def delete_volunteer(Vol_ID):
+	"""This function will delete a volunteer from the database.
+	"""
+	vol = s.query(Volunteer).filter(Volunteer.id == Vol_ID).one()
+	s.delete(vol)
+	s.commit()
+	
+	
 def age(dob):
 	"""This function calculates a person's age using the dob input to it.
 	"""
